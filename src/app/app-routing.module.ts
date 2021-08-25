c9afb1c17c9cfcc9845616d46c563126419e64dd
jhongmez <gmezjhon@gmail.com>
2021-08-25 10:51:06 -0500

    Redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
   { 
     path: 'home', 
     loadChildren: () => import('./components/home/home.module')
