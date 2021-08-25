324e6cb634572564540fa1614d6e23823e0007c9
jhongmez <gmezjhon@gmail.com>
2021-08-25 13:40:18 -0500

    Validate form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	submitted = false;
+	public formContact: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor(private formBuilder: FormBuilder) { }
 
+	ngOnInit(): void {
+
+		this.formContact = this.formBuilder.group({
+			name: ['', 
+				[
+					Validators.required,
+					Validators.minLength(3)
+				]
+			],
+			lastName: ['', 
+				[
+					Validators.required,
+					Validators.minLength(3)
+				]
+			],
+			email: ['', 
+				[
+					Validators.required,
+					Validators.email,
+					Validators.minLength(3)
+				]
+			],
+			birthdate: ['', 
+				[
+					Validators.required,
+					Validators.minLength(3)
+				]
+			],
+			terms: ['', 
+				[
+					Validators.required,
+					Validators.requiredTrue
+				]
+			],
+		})
+
+	}
+
+	//controls: Manejo de errores
+	get f() { return this.formContact.controls; }
+
+	onSubmit() {
+
+		this.submitted = true;
+  
+		// Si el formulario no es valido
+		if (this.formContact.invalid) {
+			return;
+		}
+
+	}
+	
 }
+
